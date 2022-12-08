c5a36d2fafebd0589953c711c8e8c1d55ea6c131
Florent Béjina <florent.bejina@pwc.com>
2022-12-08 16:50:21 +0100

    1:2

---
+++ 2022_12_1/2.mjs
@@ -0,0 +1,26 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const inputFilePath = path.resolve(__dirname, "input.txt");
+
+const text = (await fs.readFileSync(inputFilePath)).toString();
+
+const add = (acc, val) => acc + val;
+const sort = (a, b) => b - a;
+
+const sum = text
+  .split("\n\n")
+  .map((t) =>
+    t
+      .split("\n")
+      .map((s) => Number(s))
+      .reduce(add, 0)
+  )
+  .sort(sort)
+  .slice(0, 3)
+  .reduce(add, 0);
+
+console.log(sum);
